d457a9cea4472606aa1944c77880732defeb8f4f
anto @ aleph <trimarsanto@gmail.com>
2022-05-23 17:32:10 +0700

    modify for using VectorData and extending dataset for stratified samples

--- lkc_bisnp/scripts/train.py
+++ lkc_bisnp/scripts/train.py
@@ -7,6 +7,7 @@ import joblib as jlib
 from lkc_bisnp.lib.utils import cerr
 from lkc_bisnp.lib.reader import GenotypeVectorizer, read_barcode
 from lkc_bisnp.lib.classifier import BialleleLK, NaNBernoulliNB, CalibratedClassifierCVExt
+from lkc_bisnp.lib.metrics import prepare_stratified_samples
 
 
 def init_argparser(p=None):
@@ -35,7 +36,7 @@ def train(args):
 
     # read training barcode
     train_barcodes, train_Y = read_barcode(args.infile, read_class=args.classfile is None)
-    train_X, train_mask, logs = vectorizer.vectorize(train_barcodes)
+    train_data = vectorizer.vectorize(train_barcodes)
 
     # read training class file and replace train_Y, if necessary
     if args.classfile:
@@ -45,17 +46,20 @@ def train(args):
                 train_Y.append(line.strip())
         train_Y = np.array(train_Y)
 
+    cv = 1
     match args.method:
         case 'balk':
             clf = BialleleLK()
         case 'bnb':
             clf = NaNBernoulliNB()
         case 'iso-balk':
-            clf = CalibratedClassifierCVExt(BialleleLK(), cv=4, method='isotonic')
+            clf = CalibratedClassifierCVExt(BialleleLK(), cv=5, method='isotonic')
+            cv = 5
         case _:
             raise ValueError(f'unknown method: {args.method}')
 
-    clf.fit(train_X, train_Y)
+    X, y = prepare_stratified_samples(train_data.X, train_Y, cv)
+    clf.fit(X, y)
 
     jlib.dump((vectorizer, clf), args.outfile)
     cerr(f'[INFO - writing classifier to {args.outfile}]')
