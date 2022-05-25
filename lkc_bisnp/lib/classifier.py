4fc417ee38381efdb0ca7e8732e3daf652708631
anto @ aleph <trimarsanto@gmail.com>
2022-05-25 15:35:39 +0700

    clean up unused data

--- lkc_bisnp/lib/classifier.py
+++ lkc_bisnp/lib/classifier.py
@@ -9,18 +9,10 @@ from sklearn.naive_bayes import BernoulliNB
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.utils.extmath import safe_sparse_dot
 
-from .lkest import SNPProfile, SNPLikelihoodEstimator
+from lkc_bisnp.lib.utils import cerr, cout
 
 import logging
 log = logging.getLogger(__name__)
-
-
-def cerr(text):
-    print(text, file=sys.stderr)
-
-
-def cout(text):
-    print(text)
 
 
 class _MethodExt(object):
@@ -170,83 +162,8 @@ class BialleleLK(NaNBinomialNB):
 
 
 class CalibratedClassifierCVExt(CalibratedClassifierCV, _MethodExt):
+    """ Extend CalibratredClassifierCV with _MethodExt """
     pass
 
-
-# put the initial data here
-
-_CLASSIFIERS_ = {}
-
-
-def set_classifiers(classifiers):
-
-    global CLASSIFIERS
-    _CLASSIFIERS_ = classifiers
-
-
-def get_classifiers():
-    return _CLASSIFIERS_
-
-
-def init_classifiers(profilepath):
-
-    with open(profilepath, 'rb') as f:
-        profile_data = pickle.load(f)
-
-    _classifiers_ = get_classifiers()
-
-    for code in profile_data:
-        profile = SNPProfile.from_dict(profile_data[code])
-        _classifiers_[code] = SNPLikelihoodEstimator(profile)
-
-    log.info('Classifiers initialized from profile {}'.format(profilepath))
-
-
-def prepare_data(datalines, snpset):
-    sample_ids = []
-    haplotypes = []
-    logs = []
-
-    classifier = get_classifiers()[snpset]
-    positions = classifier.get_profile().positions
-
-    for idx, tokens in enumerate(datalines):
-        barcode = tokens[0]
-        sample_id = tokens[1]
-
-        if len(barcode) != len(positions):
-            logs.append(
-                f'Sample {sample_id} had incorrect {len(barcode)} SNPs instead of '
-                'the required {len(positions)} SNPs'
-            )
-            continue
-
-        # check barcode
-        barcode = barcode.upper()
-        haplotype = []
-        hets = []
-        masks = []
-        mask = 0
-        for allel, position in zip(barcode, classifier.get_profile().positions):
-            if allel in ['X', 'N']:
-                haplotype.append(1)
-            elif allel == position[2]:
-                haplotype.append(0)
-            elif allel == position[3]:
-                haplotype.append(2)
-            else:
-                haplotype.append(1)
-                mask += 1
-        sample_ids.append(sample_id)
-        haplotypes.append(haplotype)
-        if mask > 0:
-            logs.append(f'Sample {sample_id} was masked for {mask} SNPs')
-
-    haplotypes = numpy.array(haplotypes, dtype=numpy.int8)
-    return (haplotypes, sample_ids, logs)
-
-
-def classify(haplotypes):
-    pass
 
 # EOF
